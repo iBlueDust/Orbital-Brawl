f6ce2f051665b0d1fb01450c1b0349b817493909
Peter R. Raozen <peterraozen@gmail.com>
2020-07-11 12:35:32 +0700

    Added basic movement and controls
    Rigidbody-based movement is really disorienting. It is better-fit for the endgame.
    
    12:35
    11/7/20

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using PlayerID = System.UInt16;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[RequireComponent(typeof(ShipController))]
+public class PlayerController : MonoBehaviour {
+	public PlayerID playerId = 0;
+
+	private Controls controls;
+	private ShipController ship;
+
+	void Awake() {
+		controls = new Controls();
+		ship = GetComponent<ShipController>();
+
+		if (playerId == 0) {
+			controls.Player1.Fire.performed += _ => ship.Fire();
+		} else {
+			controls.Player2.Fire.performed += _ => ship.Fire();
+
+		}
+	}
+
+	void Update() {
+		float thrust = controls.Player1.Thrust.ReadValue<float>();
+		float steer = controls.Player1.Steer.ReadValue<float>();
+
+		Debug.Log($"Thrust: {thrust}, Steer: {steer}");
+
+		ship.Move(new Vector2(steer, thrust));
+	}
+
+	void OnEnable() {
+		controls.Enable();
+	}
+	void OnDisable() {
+		controls.Disable();
+	}
 }
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipController : MonoBehaviour {
+	public GameObject bullet;
+
+	[Header("Configuration")]
+	public float moveSpeed = 10f;
+	public float steerSpeed = 10f;
+	public float bulletSpeed = 100f;
+	public float steerDampTime = 0.5f; // 500ms?
+
+	private float steerVelocity = 0f; // For Mathf.SmoothDamp
+
+	private Rigidbody2D rigidbody;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+
+	void FixedUpdate() {
+
+	}
+
+	public void Thrust(float input) {
+		// rigidbody.AddRelativeForce(Vector2.up * Time.deltaTime * moveSpeed);
+		rigidbody.velocity = transform.up * moveSpeed * input;
+	}
+
+	public void Steer(float input) {
+		// rigidbody.AddTorque(input * Time.deltaTime * steerSpeed);
+		rigidbody.angularVelocity = -input * steerSpeed;
+	}
+
+	public void Move(Vector2 input) {
+		if (input.sqrMagnitude > 1)
+			input.Normalize();
+
+		if (input.x != 0f || input.y != 0f) {
+			// Unity uses degrees for transforms; In Mathf.Atan2, 0deg = Right
+			float degrees = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg + 90f;
+			rigidbody.rotation = Mathf.SmoothDampAngle(rigidbody.rotation, degrees, ref steerVelocity, steerDampTime, float.PositiveInfinity);
+		}
+		input *= moveSpeed * Time.deltaTime;
+		rigidbody.position += input;
+	}
+
+	public void Fire() {
+		var b = Instantiate(bullet, transform.position, transform.rotation);
+		b.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+	}
+}
