e225c9af32ac7f5b1875b7091287fedbefbd525b
Peter R. Raozen <peterraozen@gmail.com>
2020-08-01 19:26:44 +0700

    Release: 1.1.0
    Added main menu screen.
    Added randomized spawn positions.
    Tweaked menu and UI screens.
    
    Built for WebGL and Standalone.
    
    1/8/20

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+
+using Random = UnityEngine.Random;
 
 public enum GameState {
 	Running,
@@ -23,7 +25,10 @@ public class Game : MonoBehaviour {
 	}
 
 	public static Game instance;
-	public GameState _state = GameState.BeforeStart;
+	public GameState _state = GameState.Menu;
+
+	public SceneReference mainMenuScene;
+	public SceneReference gameScene;
 
 	public event Action<GameState> onStateChange = delegate { };
 	public event Action<int> onPlayerWin = delegate { };
@@ -36,10 +41,16 @@ public class Game : MonoBehaviour {
 		else {
 			Debug.LogWarning("Another running Game instance has been found. Disabling self...");
 			this.enabled = false;
+			return;
 		}
 
 		controls = new Controls();
-		controls.Enable();
+	}
+
+	void Start() {
+		if (Debug.isDebugBuild)
+			onStateChange += state => Debug.Log("GameState change: " + state);
+
 		controls.Menu.Pause.performed += _ => {
 			// Double check in case GameState = BeforeStart
 			switch (state) {
@@ -53,26 +64,79 @@ public class Game : MonoBehaviour {
 					break;
 			}
 		};
-	}
 
-	void Start() {
-		if (Debug.isDebugBuild)
-			onStateChange += state => Debug.Log("GameState change: " + state);
+		FindPlayerSpawns();
 
 		// This might be a nightmare in the future but
 		// If you wanna change the default timeScale, go here :)))
 		Time.timeScale = 0f;
 		onStateChange += state => Time.timeScale = state == GameState.Running ? 1f : 0f;
 
-		StartCoroutine(AwaitFirstInput());
+		onStateChange += state => {
+			if (state == GameState.BeforeStart) {
+				StartCoroutine(AwaitFirstInput());
+				ChoosePlayerSpawn();
+			}
+		};
+		if (state == GameState.BeforeStart) {
+			StartCoroutine(AwaitFirstInput());
+			ChoosePlayerSpawn();
+		}
 	}
 
 	IEnumerator AwaitFirstInput() {
+#if UNITY_WEBGL
+		while (Keyboard.current == null || !Keyboard.current.anyKey.isPressed) {
+			yield return null;
+		}
+#else
 		while (!Keyboard.current.anyKey.isPressed) {
 			yield return null;
 		}
+#endif
 
 		state = GameState.Running;
+	}
+
+	private struct PlayerSpawnArea {
+		public Vector3 camera;
+		public Vector3[] players;
+	}
+
+	private PlayerSpawnArea[] playerSpawns;
+	public void FindPlayerSpawns() {
+		playerSpawns = GameObject.FindGameObjectsWithTag("Spawn Area")
+			.Select(g => {
+				var spawnArea = new PlayerSpawnArea();
+				spawnArea.players = new Vector3[2];
+				spawnArea.camera = g.transform.position;
+
+				foreach (Transform child in g.transform) {
+					switch (child.tag) {
+						case "P1 Spawn":
+							spawnArea.players[0] = child.position;
+							break;
+						case "P2 Spawn":
+							spawnArea.players[1] = child.position;
+							break;
+					}
+				}
+
+				return spawnArea;
+			})
+			.ToArray();
+	}
+
+	public void ChoosePlayerSpawn() {
+		var players = GameObject.FindGameObjectsWithTag("Player");
+
+		var p1 = players.FirstOrDefault((p) => p.name == "Player 1") ?? players[0];
+		var p2 = players.FirstOrDefault((p) => p.name == "Player 2") ?? players[1];
+
+		var spawnArea = playerSpawns[Random.Range(0, playerSpawns.GetLength(0))];
+		p1.transform.SetPositionAndRotation(spawnArea.players[0], Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+		p2.transform.SetPositionAndRotation(spawnArea.players[1], Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+		Camera.main.transform.position = new Vector3(spawnArea.camera.x, spawnArea.camera.y, Camera.main.transform.position.z);
 	}
 
 	public void OnPlayerDeath(GameObject player, int playerId) {
@@ -80,17 +144,34 @@ public class Game : MonoBehaviour {
 		onPlayerWin(playerId == 0 ? 1 : 0);
 	}
 
+	public void OnEnable() {
+		controls.Enable();
+	}
+
+	public void OnDisable() {
+		controls.Disable();
+	}
+
+	#region UI Handlers
+
 	public void Pause() {
 		state = GameState.Paused;
 	}
+
 	public void Resume() {
 		state = GameState.Running;
 	}
+
 	public void Restart() {
 		Debug.Log("Restart");
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		SceneManager.LoadScene(gameScene.ScenePath);
+		state = GameState.BeforeStart;
 	}
-	public void Quit() {
-		Application.Quit(0);
+
+	public void MainMenu() {
+		SceneManager.LoadScene(mainMenuScene.ScenePath);
+		state = GameState.Menu;
 	}
+
+	#endregion
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public SceneReference gameScene;
+
+	public void Play() {
+		SceneManager.LoadScene(gameScene.ScenePath);
+		Game.state = GameState.BeforeStart;
+	}
+
+	public void Quit() {
+		Application.Quit(0);
+	}
+}
